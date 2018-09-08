12b96e0f5c6fd224a35e660e0172cc75a16f12e7
Ali Usmani <37000081+wintersoil@users.noreply.github.com>
2018-09-07 23:35:23 -0500

    Create script.js

---
+++ SurveyPage/script.js
@@ -0,0 +1,3 @@
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js');
+document.head.appendChild(jQueryScript);
